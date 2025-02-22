db0bad02b9d1aec64509baa99f635214423735c0
Ida <77254066+idaocracy@users.noreply.github.com>
2025-02-22 12:37:56 +0900

    updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://idaocracy.github.io',
+  site: 'https://www.idaocracy.com',
   integrations: [mdx(), sitemap(), tailwind()]
 });
