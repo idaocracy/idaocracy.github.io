36f7bfff2220c491344651e6004c578bf4139b8a
idaocracy <ida.immila@gmail.com>
2025-03-20 18:59:44 +0700

    test without astro seo

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Idaocracy';
-export const SITE_DESCRIPTION = 'Video game programmer, developer, technical & 3D artist';
+export const SITE_TITLE = 'Idaocracy - Visual novel developer | Unity & C# programmer | Technical artist | 3D artist | Content Creator';
+export const SITE_DESCRIPTION = "A highly versatile game developer, Unity & C# programmer, technical and 3D artist, I love to express myself creatively and promote creativity through social media, livestreaming and Youtube content creation"
 export const GENERATE_SLUG_FROM_TITLE = true
 export const TRANSITION_API = true
