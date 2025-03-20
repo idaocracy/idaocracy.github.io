2839300fece6a4b09e0aea15d6e6bafb31655236
idaocracy <ida.immila@gmail.com>
2025-03-20 20:59:10 +0700

    Added transition test

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Idaocracy - Visual novel developer | Unity & C# programmer | Technical artist | 3D artist | Content Creator';
-export const SITE_DESCRIPTION = "A highly versatile game developer, Unity & C# programmer, technical and 3D artist, I love to express myself creatively and promote creativity through social media, livestreaming and Youtube content creation"
+export const SITE_TITLE = 'Idaocracy - video game developer, artist and virtual assistant';
+export const SITE_DESCRIPTION = "With skills in both tech and art, I can tackle various projects from the ground up."
 export const GENERATE_SLUG_FROM_TITLE = true
 export const TRANSITION_API = true
